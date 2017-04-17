8a277aa1e894fdc951c7af6ec8df48baeb53139b
Rabah Zeineddine <Rabah@Rabahs-MacBook-Pro.local>
2017-04-17 10:38:37 -0300

    Remove duplication added

--- public/scripts/json_translator.js
+++ public/scripts/json_translator.js
@@ -4,63 +4,76 @@ var newJSON = {};
 var cont = 1;
 var translate_length = 1;
 
-
-
-
-function getObjectKeys(json,keysToTranslate,keys,i){
+function getObjectKeys(json, keysToTranslate, keys, i) {
     if (i == keys.length) return 0;
     if (json[keys[i]] !== null && json[keys[i]].constructor === stringConstructor && typeof json[keys[i]] !== "object") {
-            keysToTranslate[keys[i]] = false;
-            return getObjectKeys(json,keysToTranslate, keys, i + 1);
+        keysToTranslate[keys[i]] = false;
+        return getObjectKeys(json, keysToTranslate, keys, i + 1);
     }
     else if (json[keys[i]] != null && (json[keys[i]].constructor === jsonConstructor || json[keys[i]].constructor === arrayConstructor)) {
-        if(json[keys[i]].constructor === jsonConstructor) keysToTranslate[keys[i]] = {};
+        if (json[keys[i]].constructor === jsonConstructor) keysToTranslate[keys[i]] = {};
         else keysToTranslate[keys[i]] = [];
-        
-        return getObjectKeys(json[keys[i]],keysToTranslate[keys[i]],Object.keys(json[keys[i]]), 0) + getObjectKeys(json,keysToTranslate, keys, i + 1);
+        return getObjectKeys(json[keys[i]], keysToTranslate[keys[i]], Object.keys(json[keys[i]]), 0) + getObjectKeys(json, keysToTranslate, keys, i + 1);
     }
     else {
         //For boolean value and others
         keysToTranslate[keys[i]] = false;
-        return getObjectKeys(json,keysToTranslate, keys, i + 1);
+        return getObjectKeys(json, keysToTranslate, keys, i + 1);
     }
 }
-
 var div_keys = document.getElementById("div_keys");
 
-function showObjectKeys(keysToTranslate,keys,i){
-    if(i == keys.length) return 0;
-    if(keysToTranslate[keys[i]] !== null && keysToTranslate[keys[i]].constructor !== jsonConstructor && keysToTranslate[keys[i]].constructor !== arrayConstructor){
-        div_keys.innerHTML += '<input type="checkbox"/>'+keys[i]+' </br>';
-        return showObjectKeys(keysToTranslate,keys,i+1);
-    }else if(keysToTranslate[keys[i]] != null && (keysToTranslate[keys[i]].constructor === jsonConstructor || keysToTranslate[keys[i]].constructor === arrayConstructor)){
-        div_keys.innerHTML += 'Object: '+keys[i]+'</br>';
-        return showObjectKeys(keysToTranslate[keys[i]],Object.keys(keysToTranslate[keys[i]]),0)+showObjectKeys(keysToTranslate,keys,i+1);
-    }else{
-        return showObjectKeys(keysToTranslate,keys,i+1);
+function showObjectKeys(keysToTranslate, keys, i) {
+    if (i == keys.length) return 0;
+    if (keysToTranslate[keys[i]] !== null && keysToTranslate[keys[i]].constructor !== jsonConstructor && keysToTranslate[keys[i]].constructor !== arrayConstructor) {
+        div_keys.innerHTML += '<input type="checkbox"/>' + keys[i] + ' </br>';
+        return showObjectKeys(keysToTranslate, keys, i + 1);
+    }
+    else if (keysToTranslate[keys[i]] != null && (keysToTranslate[keys[i]].constructor === jsonConstructor || keysToTranslate[keys[i]].constructor === arrayConstructor)) {
+        div_keys.innerHTML += 'Object: ' + keys[i] + '</br>';
+        return showObjectKeys(keysToTranslate[keys[i]], Object.keys(keysToTranslate[keys[i]]), 0) + showObjectKeys(keysToTranslate, keys, i + 1);
+    }
+    else {
+        return showObjectKeys(keysToTranslate, keys, i + 1);
     }
 }
 
-
-function removeDuplicate(json,keys,i){
-    if(i == keys.length) return 0;
-    
-    
+function removeDuplicate(json, keys, i) {
+    if (i == keys.length) return 0;
+    if (json[keys[i]] !== null && json[keys[i]].constructor === arrayConstructor) {
+        for (var each in json[keys[i]]) {
+            var howmany = customStr(JSON.stringify(json[keys[i]])).match(new RegExp(customStr(JSON.stringify(json[keys[i]][each])), "g"));
+            //            console.log(howmany);
+            if (howmany != null) {
+                json[keys[i]].splice(each, howmany.length - 1);
+                if(howmany[0] == "sim") console.log(howmany);
+            }
+            if (json[keys[i]][each] !== undefined && json[keys[i]][each].constructor === jsonConstructor) {
+                removeDuplicate(json[keys[i]][each], Object.keys(json[keys[i]][each]), 0);
+            }
+        }
+        removeDuplicate(json, keys, i + 1);
+    }
+    else {
+        removeDuplicate(json, keys, i + 1);
+    }
 }
 
+function customStr(str) {
+    return str.replace(new RegExp("\\[", "g"), "").replace(new RegExp("\\]", "g"), "").replace(new RegExp("\\?", "g"), "").replace(new RegExp("\\{", "g"), "").replace(new RegExp("\\}", "g"), "");
+}
 
 function translateIt(json) {
     cont = 1; // for adding a new json without refresh
     json.finished = 'true';
     originalJSON = json;
     var keys = Object.keys(originalJSON);
-    getObjectKeys(originalJSON,keysToTranslate,Object.keys(originalJSON),0);
-    console.log(JSON.stringify(keysToTranslate));
-//    showObjectKeys(keysToTranslate,Object.keys(keysToTranslate),0);
-    removeDuplicate(originalJSON,Object.keys(originalJSON),0)
-//    translate_length = getKeysLength(originalJSON, keys, 0);
-//    console.log('Object has ' + translate_length + ' keys that contains value');
-//    iterateAndTranslate(originalJSON, newJSON, keys, 0);
+    //    getObjectKeys(originalJSON,keysToTranslate,Object.keys(originalJSON),0);
+    //    showObjectKeys(keysToTranslate,Object.keys(keysToTranslate),0);
+    translate_length = getKeysLength(originalJSON, keys, 0);
+    console.log('Object has ' + translate_length + ' keys that contains value');
+    iterateAndTranslate(originalJSON, newJSON, keys, 0);
+    //    removeDuplicate(originalJSON,Object.keys(originalJSON),0);
 }
 var stringConstructor = "test".constructor;
 var arrayConstructor = [].constructor;
@@ -69,7 +82,7 @@ var jsonConstructor = {}.constructor;
 function getKeysLength(json, keys, i) {
     if (i == keys.length) return 0;
     if (json[keys[i]] !== null && json[keys[i]].constructor === stringConstructor && typeof json[keys[i]] !== "object") {
-        if (keys[i] != "created" && keys[i] != "updated" && keys[i] != "conditions" && json[keys[i]] != "") {
+        if (keys[i] != "updated" && keys[i] != "created" && keys[i] != "conditions" && json[keys[i]] != "" && keys[i] != "dialog_node" && keys[i] != "go_to" && keys[i] != "intent" && keys[i] != "selector") {
             return 1 + getKeysLength(json, keys, i + 1);
         }
         else return 0 + getKeysLength(json, keys, i + 1);
@@ -84,16 +97,17 @@ function getKeysLength(json, keys, i) {
 }
 
 function iterateAndTranslate(json, translatedJSON, keys, i) {
-    if (cont == translate_length && keys[i] === undefined) {
+    console.log('cont: ' + cont + ' legnth: ' + translate_length);
+    if (cont >= translate_length && keys[i] === undefined) {
         finishedIt();
         return 0;
     }
     if (i == keys.length) return 0;
     if (json[keys[i]] !== null && json[keys[i]].constructor === stringConstructor && typeof json[keys[i]] !== "object") {
-        if (keys[i] != "updated" && keys[i] != "created" && keys[i] != "conditions" && json[keys[i]] != "") {
+        if (keys[i] != "updated" && keys[i] != "created" && keys[i] != "conditions" && json[keys[i]] != "" && keys[i] != "dialog_node" && keys[i] != "go_to" && keys[i] != "intent" && keys[i] != "selector") {
             console.log('translating: ' + json[keys[i]]);
             xhrGet('/api/translate?text=' + json[keys[i]], function (data) {
-                translatedJSON[keys[i]] = data.toLowerCase();
+                translatedJSON[keys[i]] = responseTreatmente(data.toLowerCase());
                 cont = cont + 1;
                 if (cont == translate_length && keys[i] === undefined) {
                     return 0;
@@ -122,13 +136,20 @@ function iterateAndTranslate(json, translatedJSON, keys, i) {
     }
 }
 
-function finishedIt() {
-    delete newJSON['finished'];
-    alert('acabou');
+
+
+function responseTreatmente(data){
+    if(data.indexOf("? >") != -1) data = data.replace(new RegExp("\\? >","g")," ?> ");
+    return data;
 }
 
 
 
+function finishedIt() {
+    delete newJSON['finished'];
+    removeDuplicate(newJSON, Object.keys(newJSON), 0, 0);
+    alert('acabou');
+}
 $("#file").change(function (event) {
     var uploadedFile = event.target.files[0];
     if (uploadedFile.type !== "application/json") {
